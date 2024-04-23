eaff372e0e5aab99093676ae1db2c9072f8a325d
Jack Marshall <jackbmarshall@outlook.com>
2024-04-23 15:17:23 -0500

    Nikola auto commit.
    Nikola version: 8.3.0

--- conf.py
+++ conf.py
@@ -679,7 +679,7 @@ GITHUB_SOURCE_BRANCH = 'src'
 GITHUB_DEPLOY_BRANCH = 'main'
 
 # The name of the remote where you wish to push to, using github_deploy.
-GITHUB_REMOTE_NAME = 'origin'
+GITHUB_REMOTE_NAME = 'original'
 
 # Whether or not github_deploy should commit to the source branch automatically
 # before deploying.
